d153dfa37e62e26adbd81b2e415231afd2e2206d
Sunandhit-Gupta <sunandhitgupta@gmail.com>
2022-09-30 18:25:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function watch() {i++;
+    let a = new Date();
+    let b = a.getTime();
+    let c = a.getHours();
+    let d = a.getMinutes();
+    let e = a.getSeconds();
+    let l = a.getDate();
+    let m = a.getMonth();
+    let o = a.getFullYear();
+    let p = l+"/"+m+"/"+o ;
+    let j = c + ":" + d + ":" + e;
+    f = document.getElementById("ck");
+    f.innerHTML = j;
+
+    k = document.getElementById("dt");
+    k.innerHTML = "dd/mm/yy = "+ p;
+
+setTimeout(watch,0);                 
+}
+i = 0
